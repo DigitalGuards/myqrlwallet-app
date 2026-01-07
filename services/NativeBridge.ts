12900c5280808fcd40845adda0848a1f64cbc43c
moscowchill <gasgeverij@proton.me>
2026-01-07 12:24:39 +0100

    Migrate to @react-native-clipboard/clipboard package
    Fixes deprecation warning - Clipboard was extracted from react-native
    core and will be removed in a future release.

--- services/NativeBridge.ts
+++ services/NativeBridge.ts
@@ -1,5 +1,6 @@
 import { RefObject } from 'react';
-import { Alert, Share, Clipboard, Platform, Linking } from 'react-native';
+import { Alert, Share, Platform, Linking } from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
 import WebView from 'react-native-webview';
 import SeedStorageService from './SeedStorageService';
 
